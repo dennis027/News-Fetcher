293b38531fe157534bc95d0a881cbe1891b62d40
Dennis <dennis.kimani@student.moringaschool.com>
2021-06-04 23:35:24 +0300

    initial news templates

--- app/views.py
+++ app/views.py
@@ -8,5 +8,13 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    news = "they are comming to update us"
+    news = "News for a great update"
     return render_template('index.html' , news=news)
+
+@app.route('/news/<int:news_id>')
+def news(news_id):
+
+    '''
+    View movie page function that returns the news details page and its data
+    '''
+    return render_template('news.html',id = news_id)
