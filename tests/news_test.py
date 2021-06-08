caf0b8fbffc175063ce101a80f6e201d7152d50f
Dennis <dennis.kimani@student.moringaschool.com>
2021-06-09 00:28:17 +0300

    adding updates on my test

--- tests/news_test.py
+++ tests/news_test.py
@@ -10,15 +10,14 @@ class NewsTest(unittest.TestCase):
         """
         Set up method that will run before every test
         """
-        self.new_news = News("Bush Man", 
-                             "Trump apologizes for retweeting",
-                             "In an interview with ...",
-                             "https://www.washingtonpost.com/world/trump-apologizes-for-retweeting-anti-muslim-videos-from-far-right-british-group/2018/01/26/7d334e0c-0268-11e8-9d31-d72cf78dbeee_story.html",
-                             "https://www.washingtonpost.com/rf/image_1484w/2010-2019/WashingtonPost/2018/01/26/Foreign/Images/Trump_32516-08392.jpg?t=20170517",
-                             "2018-01-26T07:58:21Z")
+        self.new_news = News
+        ("NBC News", 
+        "Biden critisized for useless border thef",
+        "In an interview with the journal",
+        "https://www.nbc.com/pride-is-universal",
+        "https://www.nbc.com/the-blacklist/video/godwin-page/4370827",
+        "2021-04-03T05:25:21Z")
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_news, News))
 
-# if __name__ == '__main__':
-#     unittest.main()
