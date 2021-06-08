60704d3fad39d2d1a325b9a3c489015273d0637a
Dennis <dennis.kimani@student.moringaschool.com>
2021-06-08 13:00:36 +0300

    adding system configuration of my app

--- config.py
+++ config.py
@@ -1,30 +1,36 @@
 import os
+
 class Config:
-    '''
-    general configuration parent class
-    '''
-    NEWS_API_BASE_URL='https://newsapi.org/v2/{}?apiKey={}'
-    NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
-    SECRET_KEY= os.environ.get('SECRET_KEY')
+    
+   #General configuration parent class
+    
+    SOURCES_API_BASE_URL = 'https://newsapi.org/v2/sources?country=us&category={}&apiKey={}'
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+    CATG_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?category={}&country=us&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+
 
 class ProdConfig(Config):
     '''
-    production configuration child class
+    Production  configuration child class
 
-    Arg:
-        Config: The parent configuration class with general configuration settings
+    Args:
+        Config: The parent configuration class with General configuration settings
     '''
     pass
+
 
 class DevConfig(Config):
     '''
     Development  configuration child class
+
     Args:
         Config: The parent configuration class with General configuration settings
     '''
+
     DEBUG = True
 
-config_options={
-    'development':DevConfig,
-    'production':ProdConfig
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
 }
