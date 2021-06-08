ccb01621b425fc2bd639057f51f0206dce429b28
Dennis <dennis.kimani@student.moringaschool.com>
2021-06-09 00:22:18 +0300

    updated categoty test

--- tests/category_test.py
+++ tests/category_test.py
@@ -9,11 +9,12 @@ class CategoryTest(unittest.TestCase):
         """
         Setup method that will run before every test
         """
-        self.new_category = Category("Trump apologizes for retweeting",
-                                     "In an interview with ...",
-                                     "https://www.washingtonpost.com/world/trump-apologizes-for-retweeting-anti-muslim-videos-from-far-right-british-group/2018/01/26/7d334e0c-0268-11e8-9d31-d72cf78dbeee_story.html",
-                                     "https://www.washingtonpost.com/rf/image_1484w/2010-2019/WashingtonPost/2018/01/26/Foreign/Images/Trump_32516-08392.jpg?t=20170517",
-                                     "2018-01-26T07:58:21Z")
+        self.new_category = Category
+        ("Biden Summons Russia for meddling with US election which he won",
+        "In a press interview with Journalist at Whitehouse",
+        "https://www.nbc.com/live?brand=nbc-news",
+        "https://www.washingtonpost.com/rf/image_1484w/2010-2019/WashingtonPost/2018/01/26/Foreign/Images/Trump_32516-08392.jpg?t=20170517",
+        "2021-01-2T05:28:21Z")
     
     def test_instance(self):
         self.assertTrue(isinstance(self.new_category, Category))
