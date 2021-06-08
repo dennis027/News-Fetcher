c8653f5cd93e6d9393e9031e688b6dc9b7ba2d83
Dennis <dennis.kimani@student.moringaschool.com>
2021-06-08 13:04:59 +0300

    adding an initialisation to python

--- app/__init__.py
+++ app/__init__.py
@@ -1,28 +1,25 @@
 from flask import Flask
-from flask_bootstrap import Bootstrap
 from config import config_options
+from flask_bootstrap import Bootstrap
 
 bootstrap = Bootstrap()
 
 def create_app(config_name):
-    app=Flask(__name__)
 
-    # creating the app configuration
+    app = Flask(__name__)
+
+    # Creating the app configurations
     app.config.from_object(config_options[config_name])
 
-    # initialising flask extensions
+    # Initializing flask extensions
     bootstrap.init_app(app)
 
-    # registering the blueprint
+    # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    # stting configuratons
-    from .requests import configue_request
-    configue_request(app)
-
-    # we will add views and forms
-    return  app
-
-
-
+    # Setting config
+    from .request import configure_request
+    configure_request(app)
+    
+    return app
