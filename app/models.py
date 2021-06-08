f749ee10e2a9fcde58323467d98446700ace245e
Dennis <dennis.kimani@student.moringaschool.com>
2021-06-08 12:59:47 +0300

    adding a custom models page

--- app/models.py
+++ app/models.py
@@ -1,10 +1,35 @@
 class News:
-    '''
-    news class to define news objects
-    '''
+    
+    #News class to define News objects
+    
+
+    def __init__(self, author, title, description, url, urlToImage, publishedAt):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+
+
+class Source:
+
+    #sources class to define source objects
+
 
     def __init__(self, id, name, url):
-        self.id=id
-        self.name=name
-        self.url=url
-        
+        self.id = id
+        self.name = name
+        self.url = url
+
+class Category:
+    
+    #categories class to define category objects
+    
+
+    def __init__(self, title, description, url, urlToImage, publishedAt):
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
